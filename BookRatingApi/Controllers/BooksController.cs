b4fdcfe0a6fd7747ff9e4b4b2fe11f304dc1bd53
Mlondi <MlondiM@DESKTOP-5S3OSRJ>
2022-09-19 12:37:20 +0200

    [Modified] return object with null value

--- BookRatingApi/Controllers/BooksController.cs
+++ BookRatingApi/Controllers/BooksController.cs
@@ -27,13 +27,13 @@ namespace BookRatingApi.Controllers
                 var results = await bookBLL.GetAllBooks();
                 if (results == null)
                 {
-                    return new BookResponse { IsSuccessful = false, SatusCode = CustomStatusCodes.ClientError, Books = null, Message = CustomMessages.NoBooksFound };
+                    return new BookResponse { IsSuccessful = false, SatusCode = CustomStatusCodes.ClientError, Message = CustomMessages.NoBooksFound };
                 }
                 return new BookResponse { IsSuccessful = true, SatusCode = CustomStatusCodes.SuccessStatus, Books = results, Message = CustomMessages.BooksFound };
             }
             catch (Exception ex)
             {
-                return new BookResponse {IsSuccessful = false, SatusCode = CustomStatusCodes.ServerError, Books = null, Message = CustomMessages.ServerError};
+                return new BookResponse {IsSuccessful = false, SatusCode = CustomStatusCodes.ServerError, Message = CustomMessages.ServerError};
             }
         }
 
@@ -47,13 +47,13 @@ namespace BookRatingApi.Controllers
 
             if (results == null)
             {
-                return new BookResponse {IsSuccessful = false, SatusCode = CustomStatusCodes.ClientError, Books = null, Message = CustomMessages.NoBooksFound };
+                return new BookResponse {IsSuccessful = false, SatusCode = CustomStatusCodes.ClientError, Message = CustomMessages.NoBooksFound };
             }
-            return new BookResponse {IsSuccessful = true, SatusCode = CustomStatusCodes.SuccessStatus, Books = null, Message = CustomMessages.BooksFound };
+            return new BookResponse {IsSuccessful = true, SatusCode = CustomStatusCodes.SuccessStatus, Message = CustomMessages.BooksFound };
         }
             catch (Exception ex)
             {
-                return new BookResponse {IsSuccessful = false, SatusCode = CustomStatusCodes.ServerError, Books = null, Message = CustomMessages.ServerError};
+                return new BookResponse {IsSuccessful = false, SatusCode = CustomStatusCodes.ServerError, Message = CustomMessages.ServerError};
         }
         }
 
@@ -66,13 +66,13 @@ namespace BookRatingApi.Controllers
                 var results = await bookBLL.SearchForBookBySearchString(searchString);
                 if(results == null)
                 {
-                    return new BookResponse {IsSuccessful = false, Books = null, SatusCode = CustomStatusCodes.ClientError, Message = CustomMessages.NoBooksFound};
+                    return new BookResponse {IsSuccessful = false, SatusCode = CustomStatusCodes.ClientError, Message = CustomMessages.NoBooksFound};
                 }
                 return new BookResponse { IsSuccessful = true, Books = results, SatusCode = CustomStatusCodes.SuccessStatus, Message = CustomMessages.BooksFound };
             }
             catch (Exception ex)
             {
-                return new BookResponse { IsSuccessful = false, Books = null, SatusCode = CustomStatusCodes.ServerError, Message = CustomMessages.ServerError };
+                return new BookResponse { IsSuccessful = false, SatusCode = CustomStatusCodes.ServerError, Message = CustomMessages.ServerError };
             }
         }
 
@@ -84,17 +84,17 @@ namespace BookRatingApi.Controllers
             {
                 var results = bookBLL.InsertBook(bookDTO);
                 if(string.IsNullOrEmpty(bookDTO.Title) || string.IsNullOrEmpty(bookDTO.Author) || string.IsNullOrEmpty(bookDTO.Summary)){
-                    return new BookResponse { SatusCode = CustomStatusCodes.ClientError, Books = null, IsSuccessful = false, Message = CustomMessages.BoosksClientError };
+                    return new BookResponse { SatusCode = CustomStatusCodes.ClientError, IsSuccessful = false, Message = CustomMessages.BoosksClientError };
                 }
                 if(results == -1)
                 {
-                    return new BookResponse { SatusCode = CustomStatusCodes.SuccessStatus, Books = null, IsSuccessful = true, Message = CustomMessages.BoosksInsertSuccess };
+                    return new BookResponse { SatusCode = CustomStatusCodes.SuccessStatus, IsSuccessful = true, Message = CustomMessages.BoosksInsertSuccess };
                 }
-                return new BookResponse { SatusCode = CustomStatusCodes.InformationalStatus, Books = null, IsSuccessful = false };
+                return new BookResponse { SatusCode = CustomStatusCodes.InformationalStatus, IsSuccessful = false };
             }
             catch (Exception ex)
             {
-                return new BookResponse { SatusCode = CustomStatusCodes.ServerError, Books = null, IsSuccessful = false, Message = CustomMessages.ServerError};
+                return new BookResponse { SatusCode = CustomStatusCodes.ServerError, IsSuccessful = false, Message = CustomMessages.ServerError};
             }
         }
 
@@ -107,17 +107,17 @@ namespace BookRatingApi.Controllers
                 var results = bookBLL.UpdateBookInfo(bookInfo);
                 if (string.IsNullOrEmpty(bookInfo.Title) || string.IsNullOrEmpty(bookInfo.Author) || string.IsNullOrEmpty(bookInfo.Summary) || string.IsNullOrEmpty(bookInfo.ISBN))
                 {
-                    return new BookResponse { IsSuccessful = false, SatusCode = CustomStatusCodes.ClientError, Books = null, Message = CustomMessages.BoosksClientError };
+                    return new BookResponse { IsSuccessful = false, SatusCode = CustomStatusCodes.ClientError, Message = CustomMessages.BoosksClientError };
                 }
                 if(results == -1)
                 {
-                    return new BookResponse {IsSuccessful = true, SatusCode = CustomStatusCodes.SuccessStatus, Books = null, Message = CustomMessages.BoosksUpdateSuccess};
+                    return new BookResponse {IsSuccessful = true, SatusCode = CustomStatusCodes.SuccessStatus, Message = CustomMessages.BoosksUpdateSuccess};
                 }
-                return new BookResponse { IsSuccessful = false, SatusCode = CustomStatusCodes.InformationalStatus, Books = null, Message = CustomMessages.FailedToUpdateBook};
+                return new BookResponse { IsSuccessful = false, SatusCode = CustomStatusCodes.InformationalStatus, Message = CustomMessages.FailedToUpdateBook};
             }
             catch (Exception)
             {
-                return new BookResponse { IsSuccessful = false, SatusCode = CustomStatusCodes.ServerError, Books = null, Message = CustomMessages.ServerError };
+                return new BookResponse { IsSuccessful = false, SatusCode = CustomStatusCodes.ServerError, Message = CustomMessages.ServerError };
             }
            
         }
